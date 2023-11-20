70b9de693effe6e6c0e2318863cfde9c9079649c
Your Name <you@example.com>
2023-11-20 15:40:03 +0900

    KP-23 script.js 추가 및 탭 이름 변경

--- app/bin/www.js
+++ app/bin/www.js
@@ -8,8 +8,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
 });
-
-/*
-cd 'E:/Kongju vs Prince/Amazon RDS/login-lecture/app'
-npm start
-*/
